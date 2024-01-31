139f04358fd7807cd080f0b6f75199a66bae4c60
Surya <suryakiran3596@gmail.com>
2024-02-01 02:06:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Time Traveler
+
+Want to improve your GitHub vanity metrics ?
+Run 
+It will create a commit for every day for the last 30 days.
+
+## Commits for the last 30
+
---
+++ generate-commits.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+MAX_DAYS=30
+
+rm -rf .git
+git init
+cat  <<EOF > README.md
+# Time Traveler
+
+Want to improve your GitHub vanity metrics ?
+Run `run.sh`
+It will create a commit for every day for the last $MAX_DAYS days.
+
+## Commits for the last $MAX_DAYS
+
+EOF
+git add .
+git commit -m "Initial commit"
+
+days=$(seq $MAX_DAYS | tac)
+for day in $days ;
+do 
+    date="$day days ago"
+    message="message was committed $day days ago"
+    echo "- This: $message" >> README.md
+    git add .
+    git commit --date "$date" -m "$message"
+done
+git log --oneline | tac
+
+cat <<EOF
+
+# Now push to GitHub with something like...
+
+git remote add origin https://github.com/surya-AE/timetravelerr
+git push -u origin main
+EOF
